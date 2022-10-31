19d20173df1a55eca3f416dae4a546c4312696b3
Meveenaramadoss <101732108+Meveenaramadoss@users.noreply.github.com>
2022-10-31 15:05:04 +0200

    final

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header ('Content-Type: application/json');
+
+$db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8', 'root', '');
+$sql= "select * from item";
+$query = $db->query($sql);
+$results = $query->fetchALL(PDO::FETCH_ASSOC);
+$json = json_encode ($results, JSON_PRETTY_PRINT);
+header('HTTP/1.1 200 OK');
+print $json;
+
+/*check*/
